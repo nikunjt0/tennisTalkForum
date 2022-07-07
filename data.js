f933104ca213e227486968e150b5e2508f5536f8
Nikunj Tyagi <86315974+nikunjt0@users.noreply.github.com>
2022-07-07 16:53:20 -0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,38 @@
+var threads = [
+	{
+		title: "Thread 1",
+		author: "Ameya",
+		date: Date.now(),
+		content: "Random Post",
+		comments: [
+			{
+				author: "Nikunj",
+				date: Date.now(),
+				content: "I Agree"
+			},
+			{
+				author: "Ram",
+				date: Date.now(),
+				content: "I Disagree"
+			}
+		]
+	},
+	{
+		title: "Thread 2",
+		author: "Ameya",
+		date: Date.now(),
+		content: "Random Post 2",
+		comments: [
+			{
+				author: "Nikunj",
+				date: Date.now(),
+				content: "I Agree"
+			},
+			{
+				author: "Ram",
+				date: Date.now(),
+				content: "I Disagree"
+			}
+		]
+	}
+]
